957316ac6d8502a04a8ff349ca442dfa1f7aaee6
25sudharsan27 <25sudharsan27@gmail.com>
2024-08-30 14:15:21 +0530

    lecture 3

---
+++ lecture 3/Frequence.java
@@ -0,0 +1,18 @@
+public class Frequence {
+    public static void main(String[] args) {
+         
+        int[] arr = new int[26];
+        String st = "sudharsan";
+        
+        for(int i=0;i<st.length();i++){
+            
+            arr[(st.charAt(i)-'a')]++;
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+            System.out.println((char) (i + 'a')+" : "+arr[i]);
+            }
+        }
+        
+    }
+}
